7255b66b809c753ca1d35050878b16d3b3f467f0
Adam Bernot <adam@bernot.dev>
2024-11-01 10:46:10 -0700

    encapsulate in function

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,20 @@
-console.log('AA Hotels Miles Per Dollar Loaded');
+const showMilesPerDollar = () => {
+	const checkInDate = document.getElementById('check-in-date').value;
+	const checkOutDate = document.getElementById('check-out-date').value;
+	const dayInMs = 1000 * 60 * 60 * 24;
 
-const checkInDate = document.getElementById('check-in-date').value;
-const checkOutDate = document.getElementById('check-out-date').value;
-const dayInMs = 1000 * 60 * 60 * 24;
+	const nights = Math.round((checkOutDate - checkInDate) / dayInMs);
 
-const nights = Math.round((checkOutDate - checkInDate) / dayInMs);
+	const hotelCards = document.querySelectorAll('[data-testid="earn-pricing-aadvantage"]');
 
-const hotelCards = document.querySelectorAll('[data-testid="earn-pricing-aadvantage"]');
+	for (const hotelCard of hotelCards) {
+		const milesElem = hotelCard.querySelector('[data-testid="tier-earn-rewards"]');
+		const miles = Number(milesElem.innerText.match(/[\d,]+/)[0].replaceAll(",", ""));
 
-for (const hotelCard of hotelCards) {
-	const milesElem = hotelCard.querySelector('[data-testid="tier-earn-rewards"]');
-	const miles = Number(milesElem.innerText.match(/[\d,]+/)[0].replaceAll(",", ""));
+		const dollarsElem = hotelCard.querySelector('[data-testid="earn-price"]');
+		const dollars = Number(dollarsElem.innerText.match(/[\d,]+/)[0].replaceAll(",", ""));
 
-	const dollarsElem = hotelCard.querySelector('[data-testid="earn-price"]');
-	const dollars = Number(dollarsElem.innerText.match(/[\d,]+/)[0].replaceAll(",", ""));
-
-	const mpd = (miles/dollars).toFixed(1);
-	dollarsElem.insertAdjacentText('beforebegin', `${mpd} miles/$`);
+		const mpd = (miles/dollars).toFixed(1);
+		dollarsElem.insertAdjacentText('beforebegin', `${mpd} miles/$`);
+	}
 }
