6a18ac452e09869d7e8d4405f8d8b7f4a5e2b17e
Adam Bernot <adam@bernot.dev>
2024-11-01 10:34:32 -0700

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,18 @@
+const checkInDate = document.getElementById('check-in-date').value;
+const checkOutDate = document.getElementById('check-out-date').value;
+const dayInMs = 1000 * 60 * 60 * 24;
+
+const nights = Math.round((checkOutDate - checkInDate) / dayInMs);
+
+const hotelCards = document.querySelectorAll('[data-testid="earn-pricing-aadvantage"]');
+
+for (const hotelCard of hotelCards) {
+	const milesElem = hotelCard.querySelector('[data-testid="tier-earn-rewards"]');
+	const miles = Number(milesElem.innerText.match(/[\d,]+/)[0].replaceAll(",", ""));
+
+	const dollarsElem = hotelCard.querySelector('[data-testid="earn-price"]');
+	const dollars = Number(dollarsElem.innerText.match(/[\d,]+/)[0].replaceAll(",", ""));
+
+	const mpd = (miles/dollars).toFixed(1);
+	dollarsElem.insertAdjacentText('beforebegin', `${mpd} miles/$`);
+}
