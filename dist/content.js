4ad0c1d6c302a5cb989ad4db0985a2c076ed070b
Adam Bernot <adam@bernot.dev>
2024-11-04 20:42:57 -0700

    typescript and fixes

---
+++ dist/content.js
@@ -0,0 +1,105 @@
+"use strict";
+const selector = {
+    'hotelCard': '[data-testid="earn-pricing-aadvantage"]',
+    'resultsContainer': '[data-testid="hotel-results-list-container"]',
+    'price': '[data-testid="earn-price"]',
+    'maxEarnRate': '#max-earn-rate',
+    'tier': '[data-testid$="tier-earn-rewards"]'
+};
+const createMilesPerDollarElement = (mpd) => {
+    const elem = document.createElement("div");
+    if (mpd >= 20) {
+        elem.setAttribute("style", "color: green;");
+    }
+    const mpdFixed = mpd.toFixed(1);
+    elem.innerText = `${mpdFixed} miles/$`;
+    return elem;
+};
+const createMaxMilesPerDollarElement = (mpd) => {
+    let elem = document.querySelector(selector.maxEarnRate);
+    if (!elem) {
+        elem = document.createElement("div");
+        elem.id = "max-earn-rate";
+        let container = document.querySelector(selector.resultsContainer);
+        container?.insertAdjacentElement('beforebegin', elem);
+    }
+    const mpdFixed = mpd.toFixed(1);
+    elem.textContent = `Maximum of ${mpdFixed} miles/$ for this search.`;
+};
+const getNights = () => {
+    const checkInDate = document.getElementById('check-in-date')?.value ?? 1;
+    const checkOutDate = document.getElementById('check-out-date')?.value;
+    const dayInMs = 1000 * 60 * 60 * 24;
+    return Math.round((Date.parse(checkOutDate) - Date.parse(checkInDate)) / dayInMs);
+};
+const extractNumber = (e) => {
+    return Number(e.textContent?.match(/[\d,]+/)?.[0]?.replaceAll(",", ""));
+};
+const showMilesPerDollar = (resultsListContainer) => {
+    const nights = getNights();
+    const hotelCards = document.querySelectorAll(selector.hotelCard);
+    if (hotelCards.length === 0) {
+        console.debug("No hotels found in results.");
+        return;
+    }
+    console.log('Calculating MPD for ' + hotelCards.length + ' hotels');
+    let maxMPD = 0;
+    for (const hotelCard of hotelCards) {
+        const dollarsElem = hotelCard.querySelector(selector.price);
+        if (!dollarsElem) {
+            console.error("earn-price not found");
+            return;
+        }
+        const dollars = extractNumber(dollarsElem);
+        if (!dollars) {
+            continue;
+        }
+        const milesElems = hotelCard.querySelectorAll(selector.tier);
+        for (const milesElem of milesElems) {
+            if (!milesElem) {
+                console.error("could not find tier-earn-rewards or non-tier-earn-rewards");
+                return;
+            }
+            const miles = extractNumber(milesElem);
+            if (!miles) {
+                continue;
+            }
+            const mpd = miles / dollars / nights;
+            if (mpd > maxMPD) {
+                maxMPD = mpd;
+            }
+            milesElem.insertAdjacentElement('beforeend', createMilesPerDollarElement(mpd));
+        }
+    }
+    createMaxMilesPerDollarElement(maxMPD);
+    console.info(`Maximum miles per dollar: ${maxMPD}`);
+};
+const observeResultsList = (resultsListContainer) => {
+    showMilesPerDollar(resultsListContainer);
+    const observer = new MutationObserver(mutationList => {
+        showMilesPerDollar(resultsListContainer);
+    });
+    observer.observe(resultsListContainer, { childList: true });
+};
+const waitForElem = (elementSelector) => {
+    console.debug(`Waiting for element: ${elementSelector}`);
+    return new Promise(resolve => {
+        let elem = document.querySelector(elementSelector);
+        if (elem) {
+            return resolve(elem);
+        }
+        const observer = new MutationObserver(mutations => {
+            elem = document.querySelector(elementSelector);
+            if (elem) {
+                observer.disconnect();
+                resolve(elem);
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    });
+};
+waitForElem(selector.resultsContainer)
+    .then((resultsContainer) => {
+    observeResultsList(resultsContainer);
+})
+    .catch(console.error);
